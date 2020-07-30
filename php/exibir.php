3483bd8adcc0686f341f1f587667b34436eaee92
Edward04007 <foreverplayer95@gmail.com>
2020-07-30 14:13:44 -0300

    melhorias de logicas e outros

--- php/exibir.php
+++ php/exibir.php
@@ -18,8 +18,8 @@
         <div><a href="index.php">EdNoob</a></div>
         <div>
             <form method="POST" action="pesquisa.php">
-                    <input maxlength="50" type="text" autocomplete="off" name="pesquisa"> 
-                <button type="submit" name="pesquisar"></button>  
+                <input maxlength="50" type="text" autocomplete="off" name="pesquisa">
+                <button type="submit" name="pesquisar"></button>
             </form>
         </div>
         <nav>
@@ -32,71 +32,51 @@
     </header>
     <div class="conteudo">
         <?php
-        include_once("processamento/conexao.php");
-        $code = "SELECT * FROM mangas";
-        $resul = mysqli_query($ed, $code);
-        if($resul){
-            while( $row = mysqli_fetch_array($resul)){?>
+        include_once("processamento/conexao.php");$code = "SELECT * FROM mangas";$resul = mysqli_query($ed, $code);      if($resul){while( $row = mysqli_fetch_array($resul)){?>
         <section>
             <article>
                 <figure>
-                    <img src="../cache_upload/<?php echo $row['arquivo']?>"> 
+                    <img src="../cache_upload/<?php echo $row['arquivo']?>">
                 </figure>
                 <figcaption>
-                <div class="Div_Text_Ilustrativo">
-                    <h1>Título:</h1>
-                    <p>Autor:</p>
-                    <?php 
-                    if($row['capitulo'] == null){
-                        if(($row['volume'] == null) and ($row['capitulo'] == null)){
-                            echo"<p></p>";
-                        }else{
-                            echo"<p>Volume:</p>";
-                        }
-                    }elseif($row['volume'] == null){
-                        if(($row['volume'] == null) and ($row['capitulo'] == null)){
-                            echo"<p></p>";
-                        }else{
-                            echo"<p>Capitulo:</p>";
-                        }
-                    }
-                     ?>
-                    <!-- <p>Editora:</p> -->
-                    <p>Status:</p>
-                    <p>Gênero:</p>
+                    <div class="Div_Text_Ilustrativo">
+                        <h1>Título:</h1>
+                        <p>Autor:</p>
+                        <?php 
+                    if(($row['volume'] == null) and ($row['capitulo'] == null)){ }elseif($row['capitulo']){echo"<p>Capitulo:</p>";}elseif($row['volume']){echo"<p>Volume:</p>";}?>
+                        <!-- <p>Editora:</p> -->
+                        <p>Status:</p>
+                        <p>Gênero:</p>
                     </div>
                     <div class="Div_Text">
-                    <h1 title="<?php echo $row['nome']?>"><?php echo $row['nome']?></h1>
-                    <p><?php echo $row['autor']?></p>
-                    <p><?php echo $row['volume']?></p>
-                    <p><?php echo $row['capitulo']?></p>
-                    <!-- <p>Editora</p> -->
-                    <p><?php echo $row['statush']?></p>
-                    <p><?php echo $row['genero']?></p>
+                        <h1 title="<?php echo $row['nome']?>"><?php echo $row['nome']?></h1>
+                        <p><?php echo $row['autor']?></p>
+                        <p><?php echo $row['volume']?></p>
+                        <p><?php echo $row['capitulo']?></p>
+                        <!-- <p>Editora</p> -->
+                        <p><?php echo $row['statush']?></p>
+                        <p><?php echo $row['genero']?></p>
                     </div>
                 </figcaption>
             </article>
-                <div class="Div-Icon_Op">
-                    <a class="icon1" href="#"></a>
-                    <button class="icon2" type="submit"></button>
-                    <a class="<?php if($row['favoritos'] == 0){echo"icon3";}elseif($row['favoritos'] == 1){echo"icon4";}?>
-                    " href="processamento/favorita.php?ed=<?php echo $row['id']?>&
-                    f=<?php echo $row['favoritos']?>">
-                    </a>
-                </div>
+            <div class="Div-Icon_Op">
+                <a class="icon1" href="#"></a>
+                <button class="icon2" type="submit"></button>
+                <a class="<?php if($row['favoritos'] == 0){echo"icon3";}else{echo"icon4";}?>"href="processamento/favorita.php?ed=<?php echo $row['id']?>&f=<?php echo $row['favoritos']?>"></a>
+            </div>
         </section>
         <?php       
             }
         }
-        ?>
+            ?>
     </div>
     <footer class="rodape">
         <h1>EdNoob</h1>
-        <p>Tenha um maior controle sobre os seus animes e mangás,
-            isto é, acompanhe tudo de mais perto atráves do EdNoob.</p>
+        <p>Tenha um maior controle sobre os seus animes e mangás,isto é, acompanhe tudo de mais perto atráves do EdNoob</p>
         <div>
             <a class="Image1" href="https://github.com/Edward04007/EdNoob" title="@Edward04007" target="_blank"></a>
-            <a class="image2" href="https://www.youtube.com/channel/UCWmUztmWBOuE7zhGHkwa_Zw?view_as=subscriber" title="Canal" target="_blank"></a>
+            <a class="image2" href="https://www.youtube.com/channel/UCWmUztmWBOuE7zhGHkwa_Zw?view_as=subscriber"
+                title="Canal" target="_blank"></a>
         </div>
     </footer>
 </body>
--- php/processamento/processo.php
+++ php/processamento/processo.php
@@ -9,7 +9,6 @@ $capitu = trim($_POST['capitulo']);
 /* $editora = trim($_POST['editora']); */
 
 if((empty($capitu)) and (empty($volume))){
-
   $tipo_arquivo = substr($_FILES['foto']['name'], -4);
   $nome = md5(time()) . $tipo_arquivo;
   $dir = "../../cache_upload/";
@@ -17,22 +16,20 @@ if((empty($capitu)) and (empty($volume))){
   $sql_cod =  "INSERT INTO mangas(arquivo, datah, nome, autor, statush, genero, favoritos) VALUES('$nome',NOW(),'$titulo','$autor','$status','$genero','0')";
   $envio = mysqli_query($ed, $sql_cod);
   header('Location:../cadastrar.php');
-}elseif(empty($capitu)){
-
-  $tipo_arquivo = substr($_FILES['foto']['name'], -4);
-  $nome = md5(time()) . $tipo_arquivo;
-  $dir = "../../cache_upload/";
-  move_uploaded_file($_FILES['foto']['tmp_name'], $dir.$nome);
-  $sql_code =  "INSERT INTO mangas(arquivo, datah, nome, volume, autor, statush, genero, favoritos) VALUES('$nome',NOW(),'$titulo','$volume','$autor','$status','$genero','0')";
-  $envio = mysqli_query($ed, $sql_code);
-  header('Location:../cadastrar.php');
-}elseif(empty($volume)){
-  
+}elseif(!empty($capitu)){
   $tipo_arquivo = substr($_FILES['foto']['name'], -4);
   $nome = md5(time()) . $tipo_arquivo;
   $dir = "../../cache_upload/";
   move_uploaded_file($_FILES['foto']['tmp_name'], $dir.$nome);
   $sql_code =  "INSERT INTO mangas(arquivo, datah, nome, capitulo, autor, statush, genero, favoritos) VALUES('$nome',NOW(),'$titulo','$capitu','$autor','$status','$genero','0')";
+  $envio = mysqli_query($ed, $sql_code);
+  header('Location:../cadastrar.php');
+}elseif(!empty($volume)){
+  $tipo_arquivo = substr($_FILES['foto']['name'], -4);
+  $nome = md5(time()) . $tipo_arquivo;
+  $dir = "../../cache_upload/";
+  move_uploaded_file($_FILES['foto']['tmp_name'], $dir.$nome);
+  $sql_code =  "INSERT INTO mangas(arquivo, datah, nome, volume, autor, statush, genero, favoritos) VALUES('$nome',NOW(),'$titulo','$volume','$autor','$status','$genero','0')";
   $envio = mysqli_query($ed, $sql_code);
   header('Location:../cadastrar.php');
 }
